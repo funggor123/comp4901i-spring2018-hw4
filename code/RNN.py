e4e78814febe2d46fb50157a85bc42b7036cfdf9
lautony97 <tllauac@connect.ust.hk>
2019-04-22 18:36:19 +0800

    model

---
+++ code/RNN.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class RNNLM(nn.Module):
+    def __init__(self, args, vocab_size, target_size, embedding_matrix=None):
+        super(RNNLM, self).__init__()
+
+        self.embedding = nn.Embedding(vocab_size, args.embed_dim)
+
+        self.model = nn.RNN(args.embed_dim, args.dim_size, num_layers= args.num_layers, dropout=args.dropout)
+
+        self.linear = nn.Linear(args.dim_size, target_size)
+
+    def forward(self, x):
+        ebd = self.embedding(x.t())
+
+        rnn_out, _ = self.model(ebd)
+        hs = rnn_out[-1]
+
+        out = self.linear(hs)
+
+        return out
